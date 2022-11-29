c8816ff0e779c2302183b59beb9fabc66a3c5d9b
Guillaume-JEAN <61049198+Guillaume-JEAN@users.noreply.github.com>
2022-11-29 09:51:53 +0100

    Partie de Khalil

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import Sidebar from '../../../../WebstormProjects/PROJECT_WEB_OCRES/frontend/src/component/sidebar/Sidebar';
+import Topbar from '../../../../WebstormProjects/PROJECT_WEB_OCRES/frontend/src/component/topbar/Topbar';
+
 
 function App() {
-  return (
-    <div className="App">
-          A vous de jouer !
-    </div>
-  );
+    return (
+        <div >
+            <Topbar />
+            <div className='container'>
+                <Sidebar />
+                otherpage
+            </div>
+
+
+
+
+        </div>
+    );
 }
 
 export default App;
