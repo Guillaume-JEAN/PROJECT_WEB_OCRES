db52a8ce73afa91cc163cf9708526aa3f83e074e
khalil semlali <mohamedkhalil.semlali@edu.ece.fr>
2022-12-04 20:55:24 +0100

    j'ai presque fini la page Chercherpersonnage.jsx et Supprimerpersonnage.jsx

--- frontend/src/component/App.js
+++ frontend/src/component/App.js
@@ -1,14 +1,34 @@
 import './App.css';
 import Sidebar from './sidebar/Sidebar';
 import Topbar from './topbar/Topbar'
+import Home from './pages/home/Home'
+import Supprimerpersonnage from "./pages/supprimerpersonnage/Supprimerpersonnage";
 import Creerpersonnage from "./pages/creerpersonnage/Creerpersonnage";
+import Chercherpersonnage from "./pages/chercherpersonnage/Chercherpersonnage";
+import Votrepersonnage from "./pages/votrepersonnage/Votrepersonnage";
+import {BrowserRouter, Route,Switch} from 'react-router-dom'
+
 function App() {
     return (
         <div>
             <Topbar />
+
+
+
             <div className="container">
+                <BrowserRouter>
                 <Sidebar />
-                <Creerpersonnage />
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route  path="/Votrepersonnage" component={Votrepersonnage}/>
+                    <Route  path="/Supprimerunpersonnage/:id" component={Supprimerpersonnage}/>
+                    <Route  path="/CréerunPersonnage/:id" component={Creerpersonnage}/>
+                    <Route path="/Chercherunpersonnage" component={Chercherpersonnage}/>
+                </Switch>
+                </BrowserRouter>
+
+
+
 
             </div>
         </div>
