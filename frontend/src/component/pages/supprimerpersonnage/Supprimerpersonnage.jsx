82b165ad5b5edbf63fe81597fe4fd256fc5e641c
Guillaume-JEAN <61049198+Guillaume-JEAN@users.noreply.github.com>
2022-12-16 04:00:08 +0100

    correction supprimer personnage

--- frontend/src/component/pages/supprimerpersonnage/Supprimerpersonnage.jsx
+++ frontend/src/component/pages/supprimerpersonnage/Supprimerpersonnage.jsx
@@ -6,29 +6,26 @@ import './supprimerpersonnage.css'
 
 class Supprimerpersonnage extends React.Component {
 
-
-
-
     constructor(props)
     {
         super(props);
         this.state = {
-            id: '',
+            Nom: '',
         }
     }
 
-    upId(e)
+    upNom(e)
     {
-        this.setState({id: e.target.value})
+        this.setState({Nom: e.target.value})
     }
 
     requete()
     {
-        const devoir = {
-            id: this.state.id,
+        const Nom = {
+            Nom: this.state.Nom,
         };
 
-        axios.post('http://localhost:3001/Characters/delete', devoir)
+        axios.post('http://localhost:3001/Characters/delete', Nom)
             .then(console.log("test"))
             .catch((err) => {
                 console.log(err);
@@ -39,12 +36,12 @@ class Supprimerpersonnage extends React.Component {
     return(
         <div className="recherche">
             <form>
-                <h1 className="chercherpersonnageTitle">Suprimer  votre Personnage</h1>
+                <h1 className="chercherpersonnageTitle">Suprimer votre Personnage</h1>
                 <div className="creerpersonnageItem">
                     <label>Veuillez saisir le nom du personnage</label>
                     <input
                         className="search"
-                        value = {this.state.id} onChange={(e)=>this.upId(e)}
+                        value = {this.state.Nom} onChange={(e)=>this.upNom(e)}
                         placeholder="nom du personnage à supprimer"
                     />
                     <button className="supprimerpersonnageButton"onClick={()=>this.requete()}>Supprimer </button>
